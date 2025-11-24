ef73f05acfd26dbaafc8c1e913c61feeb92f6ba6
Edko03 <121055810+EidrianGospodinov@users.noreply.github.com>
2025-11-24 21:43:49 +0000

    ai controller move organisation

--- Assets/_script/agentController.cs
+++ Assets/_script/agentController.cs
@@ -4,23 +4,35 @@ using UnityEngine.AI;
 public class agentController : MonoBehaviour
 {
     NavMeshAgent agent;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                agent.SetDestination(hit.point);
-            }
+            ManualMoveToPoint();
+        }
+    }
+
+    private void ManualMoveToPoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
+
+    public void MoveToTarget(Vector3 targetPosition)
+    {
+        if (agent != null && agent.isActiveAndEnabled)
+        {
+            agent.SetDestination(targetPosition);
+            Debug.Log($"Agent moving to artefact at: {targetPosition}");
         }
     }
 }
